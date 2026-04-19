2bd49d97eba999469f390f2d66f2d8c38c4169c7
aaditapruthi <aadita.work@gmail.com>
2026-04-19 23:45:00 +0530

    version2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.silentemergencyalertapp"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.silentemergencyalertapp"
@@ -36,10 +32,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
